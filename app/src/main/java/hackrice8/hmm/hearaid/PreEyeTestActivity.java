426b00345fb8d9e74b0175b002cacd5f452efdfb
minhvtm99 <39894594+minhvtm99@users.noreply.github.com>
2018-09-16 04:25:55 -0500

    random letter done

---
+++ app/src/main/java/hackrice8/hmm/hearaid/PreEyeTestActivity.java
@@ -0,0 +1,30 @@
+package hackrice8.hmm.hearaid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PreEyeTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_eye_test);
+
+        final Button btnNxt = (Button) findViewById(R.id.buttonNext);
+        btnNxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRandomLetterActivity();
+            }
+        });
+    }
+
+    public void openRandomLetterActivity() {
+        Intent firstQuestionIntent = new Intent(this, RandomLetterActivity.class);
+        startActivity(firstQuestionIntent);
+
+    }
+}
